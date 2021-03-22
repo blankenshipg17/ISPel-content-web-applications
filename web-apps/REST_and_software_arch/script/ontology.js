fe382b046acc042511fa759bf99379e23ed82b67
Grayson <blankenshipg17@students.ecu.edu>
2021-03-22 01:45:53 -0400

    update

--- web-apps/REST_and_software_arch/script/ontology.js
+++ web-apps/REST_and_software_arch/script/ontology.js
@@ -27,6 +27,18 @@ var dataset = {
       
   ],
   edges:[
+  
+  {source: 0, target: 1},
+  {source: 1, target: 2},
+  {source: 2, target: 3},
+  {source: 3, target: 4},
+  {source: 3, target: 5},
+  {source: 3, target: 6},
+  {source: 4, target: 7},
+  {source: 5, target: 7},
+  {source: 6, target: 7},
+  {source: 7, target: 8}
+  
       
     
   ]
